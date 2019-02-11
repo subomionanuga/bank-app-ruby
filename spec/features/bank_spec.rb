fb1101d884d8fb14352fe44a38f6acb26b182dfa
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-02-11 14:57:21 +0000

    test: passing inital unit and feature test

--- spec/features/bank_spec.rb
+++ spec/features/bank_spec.rb
@@ -1,7 +1,8 @@
-# require ''
+require 'account'
 
 describe "Bank Balance" do
     it "Bank account has zero balance upon initialisation" do
+      account = Account.new
       expect(account.balance).to eq(0)
   end
 end
--- spec/units/account_spec.rb
+++ spec/units/account_spec.rb
@@ -1,6 +1,9 @@
 require 'account'
 
 describe "Bank Account" do
+
+  account = Account.new
+
   it "checks that an instance of account is created" do
     expect(account).to be_an_instance_of(Account)
   end
