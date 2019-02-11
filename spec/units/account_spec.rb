1d7fa410184e963a9aba2b8aa14c6f109db5147b
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-02-11 15:01:58 +0000

    test: failing unit test for deposit

--- spec/units/account_spec.rb
+++ spec/units/account_spec.rb
@@ -7,4 +7,8 @@ describe "Bank Account" do
   it "checks that an instance of account is created" do
     expect(account).to be_an_instance_of(Account)
   end
+
+  it "checks that an amount can be deposited" do
+    expect(account.deposit(100)).to increase(account.balance).by(100)
+  end
 end
