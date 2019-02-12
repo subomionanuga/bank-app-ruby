34a192e5c8c9cfc8f4c8031ec9f6d27fe66f8b9d
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-02-12 14:50:06 +0000

    test: refactored to use 'subject' instead of defining new instance

--- spec/units/transaction_spec.rb
+++ spec/units/transaction_spec.rb
@@ -1,24 +1,23 @@
 require 'transaction'
 
-describe "Transactions" do
-  entry = Transaction.new()
+describe Transaction do
 
   it "checks that a new instance of transaction is created" do
-    expect(entry).to be_an_instance_of(Transaction)
+    expect(subject).to be_an_instance_of(Transaction)
   end
 
   it "checks that time is recorded with transactions" do
-    allow(entry).to receive(:date) { "11/02/2019" }
-    expect(entry.date).to eq("11/02/2019")
+    allow(subject).to receive(:date) { "11/02/2019" }
+    expect(subject.date).to eq("11/02/2019")
   end
 
   it "checks that new instances of transaction have amounts" do
-    allow(entry).to receive(:credit) { 1000 }
-    expect(entry.credit).to eq(1000)
+    allow(subject).to receive(:credit) { 1000 }
+    expect(subject.credit).to eq(1000)
   end
 
   it "checks that the current balance is updated with transactions" do
-    allow(entry).to receive(:balance) { 1000 }
-    expect(entry.balance).to eq(1000)
+    allow(subject).to receive(:balance) { 1000 }
+    expect(subject.balance).to eq(1000)
   end
 end
