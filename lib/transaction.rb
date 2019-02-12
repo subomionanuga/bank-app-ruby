f62a9db7928368f48e947b576a543216d54d39c9
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-02-12 12:09:48 +0000

    chore: all linting offences fixed

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -2,7 +2,7 @@ class Transaction
 
   attr_reader :date, :credit, :debit, :balance
 
-  def initialize(credit = 0, debit = 0, balance)
+  def initialize(credit = 0, debit = 0, balance = 0)
     @date = Time.now.strftime("%d/%m/%Y")
     @credit = credit
     @debit = debit
--- spec/features/bank_spec.rb
+++ spec/features/bank_spec.rb
@@ -2,6 +2,10 @@ require 'account'
 
 describe "Bank Statement" do
   it "prints the expected bank statement" do
+    account = Account.new
+    account.deposit(10000)
+    account.withdraw(5000)
+    expect(account.statement).to eq(account.entries)
 
   end
 
