e320941f99ffb0f469a23f68b2ea2a87779e602e
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-02-13 11:29:38 +0000

    tests: refactored transaction tests as it does not need to be stubbed

--- spec/units/transaction_spec.rb
+++ spec/units/transaction_spec.rb
@@ -2,22 +2,24 @@ require 'transaction'
 
 describe Transaction do
 
+  entry = Transaction.new(2000, 1000)
+
   it "checks that a new instance of transaction is created" do
     expect(subject).to be_an_instance_of(Transaction)
   end
 
   it "checks that time is recorded with transactions" do
-    allow(subject).to receive(:date) { "11/02/2019" }
-    expect(subject.date).to eq("11/02/2019")
+    # allow(subject).to receive(:date) { Time.now.strftime("%d/%m/%Y") }
+    expect(subject.date).to eq(Time.now.strftime("%d/%m/%Y"))
   end
 
-  it "checks that new instances of transaction have amounts" do
-    allow(subject).to receive(:credit) { 1000 }
-    expect(subject.credit).to eq(1000)
+  it "checks that new instances of transaction can be credited" do
+    # allow(subject).to receive(:credit) { 1000 }
+    expect(entry.credit).to eq(2000)
   end
 
   it "checks that the current balance is updated with transactions" do
-    allow(subject).to receive(:balance) { 1000 }
-    expect(subject.balance).to eq(1000)
+    # allow(subject).to receive(:balance) { 1000 }
+    expect(entry.debit).to eq(1000)
   end
 end
